792f468c32db9ae2961552304c3d4c7a81525cdc
Jeremy Barnet <jbarnet27@gmail.com>
2020-01-18 15:41:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from math import hypot
+import json
+
+def partition_data_by_ids_and_sort(data_points):
+  data_by_ids = {}
+  for data_point in data_points:
+    id = data_point['id']
+    existing = data_by_ids.get(id, None)
+    if existing:
+      data_by_ids[id] = existing.append(data_point)
+    else:
+      data_by_ids[id] = [data_point]
+  for id, data in data_by_ids.items():
+    data_by_ids[id] = sorted(data, key=lambda x: x['timestamp'], reverse=True)
+  return data_by_ids
+
 
 def infer_matching_timeseries(desired_data, other_data):
   result = []
@@ -23,8 +38,7 @@ def infer_matching_timeseries(desired_data, other_data):
         result.append(item_after)
         continue
       x = (item_before['x'] * (pre_offset / time_between)) + (item_after['x'] * (post_offset / time_between))
-      y = (item_before['y'] * (pre_offset / time_between)) + (item_after['y'] * (post_offset / time_between))
-      
+      y = (item_before['y'] * (pre_offset / time_between)) + (item_after['y'] * (post_offset / time_between)) 
       results.append({
         'id': item_before['id'],
         'timestamp': desired_item['timestamp'],
@@ -43,6 +57,7 @@ def parse_overlap(desired_data, other_infered_data):
         continue
   return results
 
+
 def compute_distances(desired_overlap_data, other_infered_data):
   results = []
   for other in other_infered_data:
@@ -55,42 +70,47 @@ def compute_distances(desired_overlap_data, other_infered_data):
     
 
 def motion_compare(desired_id, data_points, min_overlap=3):
-  data_by_ids = {}
-  for data_point in data_points:
-    id = data_point['id']
-    existing = data_by_ids.get(id, None)
-    if existing:
-      data_by_ids[id] = existing.append(data_point)
-    else:
-      data_by_ids[id] = [data_point]
-  
-  for id, data in data_by_ids.items():
-    data_by_ids[id] = sorted(data, key=lambda x: x['timestamp'], reverse=True)
-  
-  desired_data = data_by_ids[desired_id]
-  
+  data_by_ids = partition_data_by_ids_and_sort(data_points)
+  desired_data = data_by_ids.get(desired_id, None)
   distances_by_id = []
-  for id, data in data_by_ids.items():
-    if id == desired_id:
-      continue
-    other_infered_data = infer_matching_timeseries(desired_data, data)
-    desired_overlap_data = parse_overlap(desired_data, other_infered_data)
-    distances = compute_distances(desired_overlap_data, other_infered_data)
-    if len(distances) < min_overlap:
-      continue
-    distances_by_id.append({
-      'id': id,
-      'distances': distances
-    })
   
-  sorted_avg_distance = sorted(
-    distances_by_id, 
-    key=lambda x: sum(x['distances'] / len(x['distances'])),
-    reverse=True
-  )
+  if desired_data:
+    for id, data in data_by_ids.items():
+      if id == desired_id:
+        continue
+      other_infered_data = infer_matching_timeseries(desired_data, data)
+      desired_overlap_data = parse_overlap(desired_data, other_infered_data)
+      distances = compute_distances(desired_overlap_data, other_infered_data)
+      if len(distances) < min_overlap:
+        continue
+      distances_by_id.append({
+        'id': id,
+        'distances': distances
+      })
+
+    sorted_avg_distance = sorted(
+      distances_by_id, 
+      key=lambda x: sum(x['distances'] / len(x['distances'])),
+      reverse=True
+    )
   
-  return map(lambda x: x['id'], sorted_avg_distance)
+  #return map(lambda x: x['id'], sorted_avg_distance)
+  return sorted_avg_distance
     
+  
+def gen_test_data():
+  data = []
+  for id in range(10):
+    for seconds in range(10):
+      data.append({
+        'id': str(id),
+        'timestamp': datetime.utcnow().replace(tzinfo=timezone.utc) + timedelta(seconds=seconds),
+        'x': seconds+id,
+        'y': seconds+id    
+      })
+
+motion_distance('1', gen_test_data())
+
  
  
   
