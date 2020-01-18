813bb6fba2389c617888063a2d4fcbda58425473
Jeremy Barnet <jbarnet27@gmail.com>
2020-01-18 16:55:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone, timedelta
 from math import hypot
-import json
 
 def partition_data_by_ids_and_sort(data_points):
   data_by_ids = {}
@@ -12,31 +11,31 @@ def partition_data_by_ids_and_sort(data_points):
     else:
       data_by_ids[id] = [data_point]
   for id, data in data_by_ids.items():
-    data_by_ids[id] = sorted(data, key=lambda x: x['timestamp'], reverse=True)
+    data_by_ids[id] = sorted(data, key=lambda x: x['timestamp'])
   return data_by_ids
 
 
 def infer_matching_timeseries(desired_data, other_data):
   results = []
   for desired_item in desired_data:
+    item_equal = None
     item_before = None
     item_after = None
     for other_item in other_data:
-      if other_item['timestamp'] <= desired_item['timestamp']:
+      if other_item['timestamp'] == desired_item['timestamp']:
+        item_equal = other_item
+        break
+      elif other_item['timestamp'] < desired_item['timestamp']:
         item_before = other_item
-      if other_item['timestamp'] >= desired_item['timestamp']:
+      elif other_item['timestamp'] > desired_item['timestamp']:
         item_after = other_item
         break
-    if item_before and item_after:
-      timebetween = (item_after['timestamp'] - item_before['timestamp']).total_seconds()
-      pre_offset = (desired_item['timestamp'] - item_before['timestamp'])
-      post_offset = (item_after['timestamp'] - desired_item['timestamp'])
-      if pre_offset == 0:
-        results.append(item_before)
-        continue
-      elif post_offset == 0:
-        results.append(item_after)
-        continue
+    if item_equal:
+      results.append(item_equal)  
+    elif item_before and item_after:
+      time_between = (item_after['timestamp'] - item_before['timestamp']).total_seconds()
+      pre_offset = (desired_item['timestamp'] - item_before['timestamp']).total_seconds()
+      post_offset = (item_after['timestamp'] - desired_item['timestamp']).total_seconds()
       x = (item_before['x'] * (pre_offset / time_between)) + (item_after['x'] * (post_offset / time_between))
       y = (item_before['y'] * (pre_offset / time_between)) + (item_after['y'] * (post_offset / time_between)) 
       results.append({
@@ -61,7 +60,7 @@ def parse_overlap(desired_data, other_infered_data):
 def compute_distances(desired_overlap_data, other_infered_data):
   results = []
   for other in other_infered_data:
-    for desired in desired_data:
+    for desired in desired_overlap_data:
       if desired['timestamp'] == other['timestamp']:
         distance = hypot(desired['x'] - other['x'], desired['y'] - other['y'])
         results.append(distance)
@@ -85,33 +84,33 @@ def motion_compare(desired_id, data_points, min_overlap=3):
         continue
       distances_by_id.append({
         'id': id,
-        'distances': distances
+        'distances': distances,
+        'average': sum(distances)/len(distances)
       })
 
-    sorted_avg_distance = sorted(
-      distances_by_id, 
-      key=lambda x: sum(x['distances'] / len(x['distances'])),
-      reverse=True
-    )
+  sorted_avg_distance = sorted(
+    distances_by_id, 
+    key=lambda x: x['average']
+  )
   
-  #return map(lambda x: x['id'], sorted_avg_distance)
   return sorted_avg_distance
     
-  
+start_time = datetime.utcnow().replace(tzinfo=timezone.utc)
 def gen_test_data():
   data = []
-  for id in range(10):
-    for seconds in range(10):
+  for id in range(2):
+    for seconds in range(3):
       data.append({
         'id': str(id),
-        'timestamp': datetime.utcnow().replace(tzinfo=timezone.utc) + timedelta(seconds=seconds),
-        'x': seconds+id,
-        'y': seconds+id    
+        'timestamp': start_time + timedelta(seconds=seconds),
+        'x': seconds,
+        'y': seconds    
       })
   return data
 
 
-motion_compare('1', gen_test_data())
+
+print(motion_compare('1', gen_test_data()))
 
  
  
