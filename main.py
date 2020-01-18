d1770da0648b856fc175321dd89f661a741d5660
Jeremy Barnet <jbarnet27@gmail.com>
2020-01-18 15:02:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from datetime import datetime
 
 def infer_matching_timeseries(desired_data, other_data):
+  result = []
+  for desired_item in desired_data:
+    item_before = None
+    item_after = None
+    for other_item in other_data:
+      if other_item.timestamp <= desired_item.timestamp:
+        item_before = other_item
+      if other_item.timestamp >= desired_item.timestamp:
+        item_after = other_item
+        break
+    if item_before and item_after:
+      timebetween = (item_after['timestamp'] - item_before['timestamp']).total_seconds()
+      pre_offset = (desired_item['timestamp'] - item_before['timestamp'])
+      post_offset = (item_after['timestamp'] - desired_item['timestamp'])
+      if pre_offset == 0:
+        result.append(item_before)
+        continue
+      elif post_offset == 0:
+        result.append(item_after)
+        continue
+      
+      
+      
   
 
 def motion_compare(desired_id, data_points):
